0ed37f4ff3b649b40920c7bebd94dd1d0103040d
2055619 <2055619@claurendeau.qc.ca>
2024-05-10 16:11:20 -0400

    Config

---
+++ src/main/java/quixotic/projects/cryptomanager/CryptoManagerApplication.java
@@ -0,0 +1,13 @@
+package quixotic.projects.cryptomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quixotic/projects/cryptomanager/CryptoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package quixotic.projects.cryptomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
