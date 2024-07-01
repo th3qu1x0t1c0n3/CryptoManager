1ccd2e2fd1ee34f1e059d9c6094a3e1ca91f6288
2055619 <2055619@claurendeau.qc.ca>
2024-07-01 16:20:53 -0400

    Possibility

--- src/main/java/quixotic/projects/cryptomanager/service/EtherService.java
+++ src/main/java/quixotic/projects/cryptomanager/service/EtherService.java
@@ -3,6 +3,7 @@ package quixotic.projects.cryptomanager.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.access.WebInvocationPrivilegeEvaluator;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,13 +20,25 @@ import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.lang.Thread.sleep;
+
 @Service
 @RequiredArgsConstructor
 public class EtherService {
     private final RestTemplate restTemplate;
     private final TokenTxRepository tokenTxRepository;
     private final TokenRepository tokenRepository;
+    private final WebInvocationPrivilegeEvaluator privilegeEvaluator;
     private ObjectMapper mapper = new ObjectMapper();
+
+    private final List<String> contractAddressList = new ArrayList<>(
+            List.of("0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+                    "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+                    "0xf715724abba480d4d45f4cb52bef5ce5e3513ccc",
+                    "0xad38255febd566809ae387d5be66ecd287947cb9",
+                    "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+                    "0xaf88d065e77c8cc2239327c5edb3a432268e5831")
+    );
 
     public EtherPriceDTO getEthPrice(WalletDTO walletDTO) {
         String url = UriComponentsBuilder.fromHttpUrl(walletDTO.getNetwork().getBaseUrl())
@@ -53,6 +66,9 @@ public class EtherService {
         Set<TokenDTO> balances = new HashSet<>();
 
         for (TokenTxDTO coin : coins) {
+            if (!contractAddressList.contains(coin.getContractAddress())) {
+                continue;
+            }
             String url = UriComponentsBuilder.fromHttpUrl(walletDTO.getNetwork().getBaseUrl())
                     .queryParam("module", "account")
                     .queryParam("action", "tokenbalance")
@@ -61,7 +77,6 @@ public class EtherService {
                     .queryParam("tag", "latest")
                     .queryParam("apikey", walletDTO.getNetwork().getApiKey())
                     .toUriString();
-
             Map<String, Object> response = restTemplate.getForObject(url, Map.class);
             if (response != null && "1".equals(response.get("status"))) {
                 String balance = (String) response.get("result");
@@ -71,9 +86,15 @@ public class EtherService {
                         .tokenSymbol(coin.getTokenSymbol())
                         .contractAddress(coin.getContractAddress())
                         .build());
+                try {
+                    sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             } else {
+                System.out.println("Error: " + response + " for " + coin.getTokenName());
                 balances.add(TokenDTO.builder()
-                        .balance(null)
+                        .balance(BigDecimal.valueOf(-1))
                         .tokenName(coin.getTokenName())
                         .tokenSymbol(coin.getTokenSymbol())
                         .contractAddress(coin.getContractAddress())
@@ -131,6 +152,11 @@ public class EtherService {
                 .queryParam("action", "tokentx")
                 .queryParam("contractaddress", contractAddress)
                 .queryParam("address", walletDTO.getAddress())
+                .queryParam("page", "1")
+                .queryParam("offset", "100")
+                .queryParam("sort", "desc")
+                .queryParam("startblock", "0")
+                .queryParam("endblock", "99999999")
                 .queryParam("apikey", walletDTO.getNetwork().getApiKey())
                 .toUriString();
 
